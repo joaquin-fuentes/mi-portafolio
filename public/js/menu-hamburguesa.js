dfb86fd0b23ab3fa238b57787a8a905a95db0ab7
joaquin_eas@hotmail.com <joaquin_eas@hotmail.com>
2021-05-25 14:30:41 -0300

    ajustes menu burguer

---
+++ public/js/menu-hamburguesa.js
@@ -0,0 +1,10 @@
+window.addEventListener("load", function() {
+
+    let nav = document.querySelector(".enlaces-menu")
+    let menu = document.querySelector(".menu-hamburguesa")
+
+   
+    menu.addEventListener("click",function(){
+        nav.classList.toggle("activado")
+    })
+})
